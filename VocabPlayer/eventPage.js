df0d57f48fd2cf81635faaefe9f56ff5d529a2f4
unknown <Administrator@VeMunuku.redmond.corp.microsoft.com>
2020-07-27 11:04:11 +0530

    code structure

---
+++ VocabPlayer/eventPage.js
@@ -0,0 +1,8 @@
+/*
+    on pausing video, 
+        extract subtitles,
+        preprocess subtitles, 
+        split them, 
+        trigger vocab UX,
+        download definitions from google.
+*/
