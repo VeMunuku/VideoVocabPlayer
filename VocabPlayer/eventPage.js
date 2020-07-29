69e2d3d4f4a05210ca2839aa5a833885e6ff973a
Neel Bhavsar <p0psh3ll@pm.me>
2020-07-29 08:31:50 +0530

    add constants.js and fix logging in events.

--- VocabPlayer/eventPage.js
+++ VocabPlayer/eventPage.js
@@ -1,25 +1,17 @@
 chrome.runtime.onConnect.addListener(function(port) {
-
     console.assert(port.name == "SubtitlesContainer");
     port.onMessage.addListener(function(msg) {
         if(msg.todo == "downloadCaptions"){
-            //alert("EventPage : "+msg.Subtitles);
+            alert("EventPage : "+msg.Subtitles);
             downloadCaptions(msg.Subtitles);
         }
     });
   });
 
-function constructUrl(word, language){
-    //return 'https://www.google.com/search?q=%22'+word+'%20meaning%20in%20'+language+'%22';
-}
-
 function downloadCaptions(Subtitles){
-    //split captions & send message to UI to display number of items to be displayed;
-        //sendDisplaySliderMessage(10);
-    //download captions from the url;
     alert("DownloadCaptions: " + Subtitles);
+    alert(consts_stopwords);
     list = Subtitles.toLowerCase().split(/[^A-Za-z]/);
-    //sendDisplaySliderMessage(list.length);
     meanings = new Set();
     list.forEach(element=> {
         getDefinition(element, meanings);
@@ -29,17 +21,15 @@ function downloadCaptions(Subtitles){
 function getDefinition(word, set){
     var endpoint = "http://hackathonbox.westus2.cloudapp.azure.com:8000/h4ck4th0n/";
     var url = endpoint + word + "/define";
-    //alert("Making request to: " + url);
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             var ret = JSON.parse(xhttp.responseText);
             console.log(ret);
             if(ret.error || ret.definitions.length == 0){
-                console.log("error");
-                set.add("error");
+                set.add(word + " : error");
             } else{
-                alert(ret.definitions[0]['definition']);
+                alert(ret.word + " : " + ret.definitions[0]['definition']);
                 set.add(ret.definitions[0]['definition']);
             }
         }
